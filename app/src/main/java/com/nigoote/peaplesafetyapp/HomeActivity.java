d8663435924f84a41b01209ed96def02e7e89aec
Enock Ndagijimana <58587259+enock-meston@users.noreply.github.com>
2021-09-05 18:13:33 +0200

    send voice to the headquatoer II

--- app/src/main/java/com/nigoote/peaplesafetyapp/HomeActivity.java
+++ app/src/main/java/com/nigoote/peaplesafetyapp/HomeActivity.java
@@ -11,6 +11,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,14 +31,21 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button logout;
     TextView viewPhone;
-    ImageView LocationImage,PoliceHelpLineImage,VoiceImage;
+    ImageView LocationImage,PoliceHelpLineImage,VoiceImage,imagePlay;
+    TextToSpeech textToSpeech;
+
     public static final String PHONE="PHONE";
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -48,6 +56,17 @@ public class HomeActivity extends AppCompatActivity {
         LocationImage = (ImageView) findViewById(R.id.imgLocation);
         PoliceHelpLineImage = (ImageView) findViewById(R.id.imgpolicehelp);
         VoiceImage = (ImageView) findViewById(R.id.imgvoice);
+        imagePlay = (ImageView) findViewById(R.id.playsound);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS){
+                    // select language
+                    int lang = textToSpeech.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
 
 //        Location Button
         LocationImage.setOnClickListener(new View.OnClickListener() {
@@ -80,6 +99,23 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 //        ends Police Button
+
+//        play my sound by click image
+        imagePlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get Edittext Vlaue
+//                Intent intent1 = getIntent();
+//                String voice = intent1.getStringExtra("voice");
+//                Intent intent2 = getIntent();
+//                String extraPhoneNumber = intent2.getStringExtra(PHONE);
+                playMethod();
+//                int speech= textToSpeech.speak(voice,TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+
+//        ends of play my sound by click image
+
 
         final SharedPreferences sharedPreferences = getSharedPreferences("UserInfo",MODE_PRIVATE);
         Intent intent1 = getIntent();
@@ -148,11 +184,59 @@ public class HomeActivity extends AppCompatActivity {
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String,String> param = new HashMap<String, String>();
                 param.put("phonenumber", phone1);
-
                 return param;
             }
         };
         requestQueue.add(request);
     } // end fo
+
+
+//    ====================
+//    ====================
+
+    //select and play voice with method
+
+    private void playMethod(){
+        ProgressDialog progressDialog = new ProgressDialog(HomeActivity.this);
+        progressDialog.setCancelable(false);
+        progressDialog.setIndeterminate(false);
+        progressDialog.setTitle("Playing My Voice");
+        progressDialog.show();
+        String url ="http://192.168.20.170:8080/personsafety/selectVoice.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(HomeActivity.this);
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    progressDialog.dismiss();
+                    JSONObject jsonobject = new JSONObject(response);
+                    JSONArray jsonarray = jsonobject.getJSONArray("Allergy");
+                    JSONObject data = jsonarray.getJSONObject(0);
+                    String sound = data.getString("Allergy");
+                    int speech= textToSpeech.speak(sound,TextToSpeech.QUEUE_FLUSH,null);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                Toast.makeText(HomeActivity.this, "Voice not played", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> param = new HashMap<String, String>();
+                Intent intent1 = getIntent();
+                String extraPhoneNumber = intent1.getStringExtra(PHONE);
+                param.put("phonenumber",extraPhoneNumber);
+                return param;
+            }
+        };
+        requestQueue.add(request);
+    }
 
 }
